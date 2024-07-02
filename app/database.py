93b9ef9818b6c5efd68b30b021fff806f31317c8
Najath Lawani <najathlawani73@gmail.com>
2024-07-02 14:50:28 +0000

    connection à la base de données

---
+++ app/database.py
@@ -0,0 +1,23 @@
+from dotenv import dotenv_values
+from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+import certifi
+
+
+from models.user import User
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+async def init_db():
+    # Load the MongoDB connection string from the environment variable MONGODB_URI
+
+    CONNECTION_STRING = os.getenv('DB_URL')
+
+    # Create a MongoDB client
+    client = AsyncIOMotorClient(CONNECTION_STRING, tlsCAFile=certifi.where())
+
+    await init_beanie(database=client.hackathon, document_models=[ User])
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+import routers.user 
+import routers.session
+from app.database import init_db
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield 
+    print("Shutting down...")
+    
+app = FastAPI(lifespan=lifespan)
+
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -10,3 +22,7 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
+
+# Ajouter les routers dédiés
+app.include_router(routers.user.router)
+app.include_router(routers.session.router)
